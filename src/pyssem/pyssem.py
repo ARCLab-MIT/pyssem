7e1d53b042b6dd74e1bd5b4ebf7ddcee2ce86b84
blueriver212 <indy.brownhall@gmail.com>
2024-03-26 17:10:48 +0000

    adding a notebook to test out ode solvers

--- src/pyssem/pyssem.py
+++ src/pyssem/pyssem.py
@@ -14,41 +14,42 @@ def main(species_json):
                     delta=10, integrator = "rk4", density_model = "static_exp_dens_func", 
                     LC=0.1, v_imp=10)
 
-    # # Create a list of species for the scene
-    # species_list = Species()
+    # Create a list of species for the scene
+    species_list = Species()
 
-    # # Import the species from a JSON file - this will be defined by the user
-    # species_list.add_species_from_json(species_json)
+    # Import the species from a JSON file - this will be defined by the user
+    species_list.add_species_from_json(species_json)
 
-    # # Pass functions for drag and PMD
-    # species_list.convert_params_to_functions()
+    # Pass functions for drag and PMD
+    species_list.convert_params_to_functions()
 
-    # # Apply Launch Rates and create symbolic variables
-    # # launch function should be constant over time, rather than a function of altitude=
-    # species_list.apply_launch_rates(scenario_properties.n_shells)
-    # species_list.create_symbolic_variables(scenario_properties.n_shells)
+    # Apply Launch Rates and create symbolic variables
+    # launch function should be constant over time, rather than a function of altitude=
+    species_list.apply_launch_rates(scenario_properties.n_shells)
+    species_list.create_symbolic_variables(scenario_properties.n_shells)
 
-    # # Pair the active species to the debris species for PMD modeling
-    # species_list.pair_actives_to_debris(species_list.species['active'], species_list.species['debris'])
+    # Pair the active species to the debris species for PMD modeling
+    species_list.pair_actives_to_debris(species_list.species['active'], species_list.species['debris'])
 
-    # # Add the final species to the scenario properties to be used in the simulation
-    # scenario_properties.add_species_set(species_list.species)
+    # Add the final species to the scenario properties to be used in the simulation
+    scenario_properties.add_species_set(species_list.species)
 
-    # # Create collision pairs
-    # #scenario_properties.add_collision_pairs(create_collision_pairs(scenario_properties))
+    # Create collision pairs
+    scenario_properties.add_collision_pairs(create_collision_pairs(scenario_properties))
 
     # Initial Population and ADEPT Launch Model 
     scenario_properties.initial_pop_and_launch()
+
+    # If scenario properties are saved, then load them
+    # with open('scenario_properties_1.pkl', 'rb') as f:
+    #     scenario_properties = pickle.load(f)
+
+    scenario_properties.build_model()
 
     # Save scenario properties is a pickle file
     with open('scenario_properties_1.pkl', 'wb') as f:
         pickle.dump(scenario_properties, f)
 
-    # If scenario properties are saved, then load them
-    with open('scenario_properties_1.pkl', 'rb') as f:
-        scenario_properties = pickle.load(f)
-
-    scenario_properties.build_model()
 
     scenario_properties.run_model()
 
