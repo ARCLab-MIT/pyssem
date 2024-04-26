fa4333f3487bf89395960565f56c2d054a565f18
unknown <indy.brownhall@gmail.com>
2024-04-26 17:13:09 +0100

    adding dist

--- setup.py
+++ setup.py
@@ -7,5 +7,22 @@ setup(
     package_data={
         'pyssem': ['utils/launch/data/*.csv'],
     },
-    # Include any other package data below
+    install_requires=[
+        "numpy==1.24.3",
+        "pandas==2.0.3",
+        "scipy==1.10.1",
+        "setuptools==68.0.0",
+        "sympy==1.11.1",
+        "tqdm==4.65.0"
+    ],
+    extras_require={
+        'dev': ['pytest', 'check-manifest'],
+        'test': ['coverage'],
+    },
+    python_requires='>=3.6',  # Define which Python versions are supported
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+    ],
 )
