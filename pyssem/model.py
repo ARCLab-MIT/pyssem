efc32561fd7394f2333fa0a069b23e8d80df949f
blueriver212 <indy.brownhall@gmail.com>
2024-07-31 11:45:59 +0100

    merging main

--- pyssem/model.py
+++ pyssem/model.py
@@ -377,49 +377,49 @@ class Model:
 
 if __name__ == "__main__":
 
-    # with open(os.path.join('pyssem', 'three_species.json')) as f:
-    #     simulation_data = json.load(f)
+    with open(os.path.join('pyssem', 'example-sim-simple.json')) as f:
+        simulation_data = json.load(f)
 
-    # scenario_props = simulation_data["scenario_properties"]
+    scenario_props = simulation_data["scenario_properties"]
 
-    # # Create an instance of the pySSEM_model with the simulation parameters
-    # model = Model(
-    #     start_date=scenario_props["start_date"].split("T")[0],  # Assuming the date is in ISO format
-    #     simulation_duration=scenario_props["simulation_duration"],
-    #     steps=scenario_props["steps"],
-    #     min_altitude=scenario_props["min_altitude"],
-    #     max_altitude=scenario_props["max_altitude"],
-    #     n_shells=scenario_props["n_shells"],
-    #     launch_function=scenario_props["launch_function"],
-    #     integrator=scenario_props["integrator"],
-    #     density_model=scenario_props["density_model"],
-    #     LC=scenario_props["LC"],
-    #     v_imp = scenario_props.get("v_imp", None), 
-    #     fragment_spreading=scenario_props.get("fragment_spreading", True),
-    #     parallel_processing=scenario_props.get("parallel_processing", False),
-    #     baseline=scenario_props.get("baseline", False),
-    #     indicator_variables=scenario_props.get("indicator_variables", None)
-    # )
+    # Create an instance of the pySSEM_model with the simulation parameters
+    model = Model(
+        start_date=scenario_props["start_date"].split("T")[0],  # Assuming the date is in ISO format
+        simulation_duration=scenario_props["simulation_duration"],
+        steps=scenario_props["steps"],
+        min_altitude=scenario_props["min_altitude"],
+        max_altitude=scenario_props["max_altitude"],
+        n_shells=scenario_props["n_shells"],
+        launch_function=scenario_props["launch_function"],
+        integrator=scenario_props["integrator"],
+        density_model=scenario_props["density_model"],
+        LC=scenario_props["LC"],
+        v_imp = scenario_props.get("v_imp", None), 
+        fragment_spreading=scenario_props.get("fragment_spreading", False),
+        parallel_processing=scenario_props.get("parallel_processing", True),
+        baseline=scenario_props.get("baseline", False),
+        indicator_variables=scenario_props.get("indicator_variables", None)
+    )
 
-    # species = simulation_data["species"]
+    species = simulation_data["species"]
 
-    # species_list = model.configure_species(species)
+    species_list = model.configure_species(species)
 
-    # results = model.run_model()
+    results = model.run_model()
 
-    # results_json = model.results_to_json()
+    results_json = model.results_to_json()
 
-    # # save the scenario properties to a pickle file
-    # with open('scenario-properties-baseline.pkl', 'wb') as f:
-    #     pickle.dump(model.scenario_properties, f)
+    # save the scenario properties to a pickle file
+    with open('scenario-properties-baseline.pkl', 'wb') as f:
+        pickle.dump(model.scenario_properties, f)
     
-    # model.create_plots()
+    model.create_plots()
 
-    with open('scenario-properties-baseline.pkl', 'rb') as f:
-        scenario_properties = pickle.load(f)
-
-    scenario_properties.cum_CSI()
-
-    # # export the results to a JSON file
+    # export the results to a JSON file
     # with open('results.json', 'w') as f:
     #     f.write(scenario_properties.results)
+
+    # with open('scenario-properties-baseline.pkl', 'rb') as f:
+    #     scenario_properties = pickle.load(f)
+
+    # output = scenario_properties.cum_CSI()
