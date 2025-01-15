43ccad9b52a6d544b7c24b5a8bd02e3f7d263a96
glavezzi <glavezzi@mit.edu>
2025-01-15 12:14:25 -0500

    optimizer test

--- pyssem/model.py
+++ pyssem/model.py
@@ -155,14 +155,15 @@ class Model:
             raise ValueError("Invalid scenario properties provided.")
         try:
 
-            self.scenario_properties.initial_pop_and_launch(baseline=self.baseline) # Initial population is considered but not launch
+            # self.scenario_properties.initial_pop_and_launch(baseline=self.baseline) # Initial population is considered but not launch
             self.scenario_properties.build_model()
 
             # save the scenario properties to a pickle file
             # with open('scenario-properties-test-no-run.pkl', 'wb') as f:
             # with open('scenario-properties-test-no-run-mult.pkl', 'wb') as f:
             # with open('scenario-properties-baseline-no-run.pkl', 'wb') as f:
-            with open('scenario-properties-test-no-run-policy.pkl', 'wb') as f:
+            # with open('scenario-properties-test-no-run-policy.pkl', 'wb') as f:
+            with open('scenario-properties-test-opt.pkl', 'wb') as f:
                 pickle.dump(self.scenario_properties, f)
             
             return self.scenario_properties
@@ -198,9 +199,10 @@ class Model:
 
 if __name__ == "__main__":
 
-    with open(os.path.join('pyssem', 'three_species.json')) as f:
+    # with open(os.path.join('pyssem', 'three_species.json')) as f:
     # with open(os.path.join('pyssem', 'three_species_mult.json')) as f:
     # with open(os.path.join('pyssem', 'baseline_amos.json')) as f:
+    with open(os.path.join('pyssem', 'three_species_opt.json')) as f:
         simulation_data = json.load(f)
 
     scenario_props = simulation_data["scenario_properties"]
