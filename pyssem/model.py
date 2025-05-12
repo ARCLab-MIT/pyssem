77868d06c9ebb0cb99277e46d9cd78443b6020d5
Indigo Brownhall <indy.brownhall@gmail.com>
2025-05-12 11:39:39 +0100

    merging main

--- pyssem/model.py
+++ pyssem/model.py
@@ -217,7 +217,7 @@ class Model:
 
 if __name__ == "__main__":
 
-    with open(os.path.join('pyssem', 'simulation_configurations', 'three_species_mult.json')) as f:
+    with open(os.path.join('pyssem', 'simulation_configurations', 'three_species_opt.json')) as f:
         simulation_data = json.load(f)
 
     scenario_props = simulation_data["scenario_properties"]
